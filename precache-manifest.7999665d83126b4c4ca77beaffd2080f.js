4ccd5ca6d6fa40dcefba2a45260ee5d957a2b28e
Ko Seoyoung <39763891+syoung125@users.noreply.github.com>
2020-03-09 22:49:15 +0900

    Updates

---
+++ precache-manifest.7999665d83126b4c4ca77beaffd2080f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ceea4b02582aec54fedf39ebcc05a07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a87e6038a03c69543273",
+    "url": "/static/css/main.9f4069f4.chunk.css"
+  },
+  {
+    "revision": "5b34aa61a648f2d64abd",
+    "url": "/static/js/2.8deb2921.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.8deb2921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87e6038a03c69543273",
+    "url": "/static/js/main.566ef1f3.chunk.js"
+  },
+  {
+    "revision": "d926f66f5294d64d9e5a",
+    "url": "/static/js/runtime-main.e0fad201.js"
+  }
+]);
